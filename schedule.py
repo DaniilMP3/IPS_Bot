0dcea1b0b45cc37c5a609ae59309a58bf9007abd
Aresmp3 <daniilpurisov390@gmail.com>
2022-11-03 15:05:09 +0200

    optimize schedule commands

--- schedule.py
+++ schedule.py
@@ -1,42 +1,29 @@
-from create_bot import bot, AVAILABLE_DAYS
+from create_bot import bot, AVAILABLE_DAYS, schedule_dict
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
 import json
 from keyboards import day_schedule_keyboard, user_keyboard, get_keyboard
 from aiogram.dispatcher import FSMContext
+from datetime import datetime
 
 
-async def show_schedule(message):
+async def show_schedule(message, day, show_current=False):
     chat_id = str(message.chat.id)
     user_id = str(message.from_user.id)
+    chat_type = message.chat.type
 
-    with open('schedule.json', 'r') as file:
-        schedule_dict = json.load(file)
-    day = message.text.split('_')[1].lower()
     if day in AVAILABLE_DAYS:
         if day in schedule_dict:
             schedule_message = schedule_dict[day]['message']
-            entities = [types.MessageEntity(type=entity['type'],
-                                            offset=entity['offset'],
-                                            length=entity['length'],
-                                            url=entity['url']) for entity in schedule_dict[day]['entities']]
 
-            if message.chat.type == 'private':
-                await bot.send_message(chat_id, schedule_message, entities=entities,
-                                        reply_markup=await get_keyboard(user_id))
-            else:
-                await bot.send_message(chat_id, schedule_message, entities=entities)
-        else:
-            if message.chat.type == 'private':
-                await bot.send_message(chat_id, "There's no lessons this day.", reply_markup=await get_keyboard(user_id))
-            else:
-                await bot.send_message(chat_id, "There's no lessons this day.")
+            await bot.send_message(chat_id, schedule_message, parse_mode='HTML', reply_markup=await get_keyboard(user_id, chat_type))
+        else: ###if admin do not edit sth for this day
+            await bot.send_message(chat_id, "There's no lessons today.", reply_markup=await get_keyboard(user_id,
+                                                                                                            chat_type))
 
     else:
-        if message.chat.type == 'private':
-            await bot.send_message(chat_id, 'Incorrect day', reply_markup=await get_keyboard(user_id))
-        else:
-            await bot.send_message(chat_id, 'Incorrect day')
+        await bot.send_message(chat_id, 'Incorrect day', reply_markup=await get_keyboard(user_id,
+                                                                                         chat_type))
 
 
 async def show_days(message: types.Message, state: FSMContext):
@@ -48,23 +35,35 @@ async def show_days(message: types.Message, state: FSMContext):
 async def show_schedule_private(message: types.Message, state: FSMContext):
     chat_id = str(message.chat.id)
     user_id = str(message.from_user.id)
+    chat_type = message.chat.type
+    day = message.text.split('_')[1].lower().split('@')[0]
     if message.text == '❌':
-        await bot.send_message(chat_id, 'You canceled choosing.', reply_markup=await get_keyboard(user_id))
+        await bot.send_message(chat_id, 'You canceled choosing.', reply_markup=await get_keyboard(user_id, chat_type))
     else:
-        await show_schedule(message)
+        await show_schedule(message, day)
     await state.finish()
 
 
 async def show_schedule_group(message: types.Message):
-    await show_schedule(message)
+    day = message.text.split('_')[1].lower().split('@')[0]
+    await show_schedule(message, day)
 
 
 
+async def show_today_schedule(message: types.Message):
+    day = datetime.now().strftime('%A').lower()
+    await show_schedule(message, day)
 
 
+async def show_current_lesson(message: types.Message):
+    datetime_instance = datetime.now()
+    day = datetime_instance.strftime('%A').lower()
+    await show_schedule(message, day, show_current=True)
 
 
 def register_schedule_handler(dp: Dispatcher):
     dp.register_message_handler(show_days, Text(equals=['/Schedule']), chat_type=[types.ChatType.PRIVATE])
     dp.register_message_handler(show_schedule_private, state='schedule_choose_day')
-    dp.register_message_handler(show_schedule_group, Text(startswith=['/Schedule_']))
+    dp.register_message_handler(show_today_schedule, Text(startswith=['/schedule_today']))
+    dp.register_message_handler(show_schedule_group, Text(startswith=['/schedule_']))
+    dp.register_message_handler(show_current_lesson, Text(startswith=['/now']))
