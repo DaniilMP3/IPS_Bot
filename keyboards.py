2368bcc68d2a6667e69b95c7d75f44e889b2aa1b
Aresmp3 <daniilpurisov390@gmail.com>
2022-11-03 15:07:51 +0200

    get lessons keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from create_bot import ADMINS
+from create_bot import ADMINS, schedule_dict
 
 user_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton(
             '/Schedule'), types.KeyboardButton('/Homework'))
@@ -7,15 +7,33 @@ user_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.Keyboa
 admin_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton(
                 '/Schedule'), types.KeyboardButton('/Homework'), types.KeyboardButton('/Edit_Schedule'))
 
-
-async def get_keyboard(user_id):
-    if user_id in ADMINS:
-        return admin_keyboard
-    else:
-        return user_keyboard
+single_cross_kb = types.ReplyKeyboardMarkup(resize_keyboard=True).add('❌')
 
 
-admin_choose_day_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('/Schedule_Monday'),
+async def get_keyboard(user_id, chat_type):
+    if chat_type == 'private':
+        if user_id in ADMINS:
+            return admin_keyboard
+        else:
+            return user_keyboard
+    return None
+
+
+async def lessons_keyboard(day, only_lessons=False):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    if day in schedule_dict:
+        for lesson in sorted(schedule_dict[day]['lessons']):
+            kb.add(f"{lesson}")
+
+    if only_lessons is True:
+        kb.add('❌')
+        return kb
+    kb.add('Create')
+    kb.add('Delete')
+    kb.add('❌')
+    return kb
+
+admin_choose_day_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Monday'),
         types.KeyboardButton('Tuesday'),
         types.KeyboardButton('Wednesday'),
         types.KeyboardButton('Thursday'),
